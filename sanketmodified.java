961e5f630118719a2ea6b12a38182ef1a376e517
sanket07 <szunjakar@gmail.com>
2021-05-26 16:51:39 +0530

    modified

---
+++ sanketmodified.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Test {
+
+	public static void main(String[] args) {
+	
+       Scanner sc=new Scanner(System.in);
+         int empid;
+ 
+ 
+ 
+        System.out.println("enter employ id");
+        empid=sc.nextInt();
+        System.out.println("employ id:"+empid);
+       
+ 
+ 
+	}
+	
+
+}
